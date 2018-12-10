6cad5f917c2c3df9168203f68fffa4641790f52a
chipallino1 <skorupich00@mail.ru>
2018-12-11 00:12:13 +0300

    get all proc for tickets

--- tickets.sql
+++ tickets.sql
@@ -24,15 +24,18 @@ create or replace procedure get_tickets_by_all(
             type_prm in varchar2
         )
         is
-        cursor get_all is select * from tickets inner join routes on(routes.id=tickets.id_route)
+        cursor get_all is select * from tickets inner join (routes inner join points on(routes.id_from=points.id)
+        inner join points on(routes.id_to=points.id)) on(routes.id=tickets.id_route)
         inner join trains on(trains.id=tickets.id_train)
         where id_train=id_train_prm or id_route=id_route_prm or place_num=place_num_prm 
         or carriage_num=carriage_num_prm or type=type_prm;
         type curr_row is record(
+            id int,
             place_num int,
             carriage_num int,
             id_route int,
             id_train int,
+            type varchar2(100),
             id_1 int,
             id_from int,
             id_to int,
@@ -61,7 +64,8 @@ create or replace procedure get_tickets_by_all(
     loop
     fetch get_all into current_row;
     exit when get_all%notfound;
-     dbms_output.put_line();
+     dbms_output.put_line('Place num: '||current_row.place_num||' Carriage num: '||current_row.carriage_num||' Type: '||current_row.type);
+     dbms_output.put_line('Train name: '||current_row.name || ' Num from: '||current_row.numeration_from);
     dbms_output.put_line('From: '||current_row.country||' '||current_row.region||' '||current_row.city||' To: '||current_row.country_1
     ||' '||current_row.region_1||' '||current_row.city_1);
     dbms_output.put_line('Length: '||current_row.length||' Date start: '||current_row.date_start||' '||current_row.date_finish);
@@ -69,7 +73,9 @@ create or replace procedure get_tickets_by_all(
     exception
         when others then dbms_output.put_line(sqlerrm);
                          rollback;
-end get_routes_by_all;
+end get_tickets_by_all;
+
+
 
 
 select * from tickets inner join (routes inner join points on(routes.id_from=points.id)
